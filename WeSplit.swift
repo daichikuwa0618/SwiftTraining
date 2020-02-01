28cde2e70fafce28287de0880c775d85dc21f973
Daichi Hayashi <daichi.hayashi.s5@dc.tohoku.ac.jp>
2020-02-01 21:18:57 +0900

    grammar review

---
+++ WeSplit.swift
@@ -0,0 +1,20 @@
+//
+//  WeSplit.swift
+//  
+//
+//  Created by Daichi Hayashi on 2020/02/01.
+//
+
+import SwiftUI
+
+struct WeSplit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeSplit_Previews: PreviewProvider {
+    static var previews: some View {
+        WeSplit()
+    }
+}
