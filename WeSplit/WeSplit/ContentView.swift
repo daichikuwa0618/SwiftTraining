f63d2b38fa8803d9d552df3cdeb06d5ea1785f01
Daichi Hayashi <daichi.hayashi.s5@dc.tohoku.ac.jp>
2020-02-01 21:24:00 +0900

    initial commit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Daichi Hayashi on 2020/02/01.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
